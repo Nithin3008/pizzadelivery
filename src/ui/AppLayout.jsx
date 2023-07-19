050f8f07c04dc2070756b3ce15a5a139d72ba471
Nithin <nithindevathi@gmail.com>
2023-07-19 11:33:32 +0530

    Added header, search, cart

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,3 +1,20 @@
+import { Header } from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router";
+import { Loader } from "./Loader";
 export const AppLayout = () => {
-  return <div>AppLayout</div>;
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="layout">
+      {isLoading && <Loader></Loader>}
+      <Header />
+      <main>
+        <h1>Content</h1>
+        <Outlet></Outlet>
+      </main>
+      <CartOverview></CartOverview>
+    </div>
+  );
 };
