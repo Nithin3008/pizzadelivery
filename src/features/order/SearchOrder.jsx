3cee1d5aa842e5cf78dd44a054dfdf43724b180d
Nithin <nithindevathi@gmail.com>
2023-07-20 14:32:13 +0530

    Added styling

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 export const SearchOrder = () => {
   const [query, setQuery] = useState();
@@ -9,7 +9,7 @@ export const SearchOrder = () => {
     event.preventDefault();
     if (query.length === 0) return;
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -17,6 +17,7 @@ export const SearchOrder = () => {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="foucs:ring-opacity-50 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
       ></input>
     </form>
   );
