d6293ea68c2249c917c1a4a9c9cfbfe0eca0d4ee
Nithin <nithindevathi@gmail.com>
2023-07-20 18:48:01 +0530

    Users can see total price, number of items

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getTotalCartPrice, getTotalCartQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
   //do this type of calculation in useselector itself
-  const totalCartQuantity = useSelector((state) =>
-    state.cart.cart.reduce((acc, item) => acc + item.quantity, 0),
-  );
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>$23.45</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
