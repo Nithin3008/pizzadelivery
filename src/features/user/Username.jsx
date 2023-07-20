9473c1dd278ccd30882999a2e2043c5a77631d03
Nithin <nithindevathi@gmail.com>
2023-07-20 15:14:14 +0530

    Now users can use appln using Name

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,9 @@
+import { useSelector } from 'react-redux';
+
 export const Username = () => {
-  return <div className="hidden text-sm font-semibold md:block">Username</div>;
+  const userName = useSelector((state) => state.user.userName);
+  if (!userName) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 };
