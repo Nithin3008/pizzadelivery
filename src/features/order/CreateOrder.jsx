8b60b5766daaa5c281d21e2d3ec17e13cca2614a
Nithin <nithindevathi@gmail.com>
2023-07-21 11:14:40 +0530

    Users can successfully place new orders

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,44 +1,28 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import { Button } from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import EmptyCart from '../cart/EmptyCart';
+import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import store from '../../store';
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData(); //here we are getting the error data from below function because there two are connected using action property in router
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
-
+  const [withPriority, setWithPriority] = useState(false);
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const userName = useSelector((state) => state.user.userName);
+  if (!cart.length) return <EmptyCart></EmptyCart>;
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
@@ -51,6 +35,7 @@ function CreateOrder() {
               className="input w-full"
               type="text"
               name="customer"
+              defaultValue={userName}
               required
             />
           </div>
@@ -87,8 +72,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
@@ -113,13 +98,14 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone = 'please give valid phone number';
   if (Object.keys(errors).length > 0) return errors;
   //everything clear then it will redirect
+  store.dispatch(clearCart());
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
