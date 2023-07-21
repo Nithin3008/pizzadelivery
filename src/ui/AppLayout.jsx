23fdf4481ab19b537fad37ca8953e87e9ff3adc6
Nithin <nithindevathi@gmail.com>
2023-07-21 11:23:06 +0530

    Fixed styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,20 @@
-import { Header } from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router";
-import { Loader } from "./Loader";
+import { Header } from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router';
+import { Loader } from './Loader';
 export const AppLayout = () => {
   const navigation = useNavigation();
   console.log(navigation);
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader></Loader>}
       <Header />
-      <main>
-        <h1>Content</h1>
-        <Outlet></Outlet>
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet></Outlet>
+        </main>
+      </div>
       <CartOverview></CartOverview>
     </div>
   );
